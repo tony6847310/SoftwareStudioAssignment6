b82c3b178faa927c02a539ea368b709701c840ff
TonyZheng <tony684310@gmail.com>
2016-05-10 01:04:27 +0800

    Draw characters

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,6 +1,7 @@
 package main.java;
 
-import processing.core.PApplet;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
 * This class is used to store states of the characters in the program.
@@ -9,15 +10,34 @@ import processing.core.PApplet;
 public class Character {
 	
 	private MainApplet parent;
-
-	public Character(MainApplet parent){
-
+	
+	private int color;
+	private float x,y;
+	private float radius = 50;
+	private ArrayList<Character> targets;
+	public HashMap<String, Integer> scenes;
+	public String name;
+	
+	public Character(MainApplet parent, String name, int color, float x, float y){
+		this.scenes = new HashMap<String, Integer>();
+		this.targets = new ArrayList<Character>();
 		this.parent = parent;
-		
+		this.name = name;
+		this.color = color;
+		this.x = x;
+		this.y = y;
 	}
 
 	public void display(){
-
+		this.parent.fill(color);
+		this.parent.ellipse(x, y, radius, radius);
 	}
 	
+	public void addTarget(Character target){
+		this.targets.add(target);
+	}
+	
+	public ArrayList<Character> getTargets(){
+		return this.targets;
+	}
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -3,6 +3,7 @@ package main.java;
 import processing.core.PApplet;
 import processing.data.JSONArray;
 import processing.data.JSONObject;
+import java.util.*;
 
 /**
 * This class is for sketching outcome using Processing
@@ -20,23 +21,45 @@ public class MainApplet extends PApplet{
 	private JSONObject ep1;
 	private JSONArray ep1_nodes;
 	private JSONArray ep1_links;
+	private ArrayList<Character> characters;
 	
 	public void setup() {
-
+		characters = new ArrayList<Character>();
+		
 		size(width, height);
 		smooth();
 		loadData();
-		
 	}
 
 	public void draw() {
+		background(255);
 		
+		noStroke();
+		for(Character c: this.characters){
+			c.display();
+		}
 	}
 
 	private void loadData(){
 		ep1 = loadJSONObject(path + file);
 		ep1_nodes = ep1.getJSONArray("nodes");
 		ep1_links = ep1.getJSONArray("links");
+		
+		for(int i=0 ; i<ep1_nodes.size(); i++){
+			JSONObject node = ep1_nodes.getJSONObject(i);
+			int color = unhex(node.getString("colour").substring(1, 8));
+			//convert hex string to int
+			characters.add(new Character(this, node.getString("name"), color, i%4*55 +50, i/4*55 + 50));
+			//3 character each row
+		}
+
+		for(int i=0; i<ep1_links.size(); i++){
+			JSONObject link = ep1_links.getJSONObject(i);
+			characters.get(link.getInt("source")).addTarget(characters.get(link.getInt("target")));
+			characters.get(link.getInt("source")).scenes.put(
+					characters.get(link.getInt("target")).name, link.getInt("value"));
+			//record scenes with each other
+		}
 	}
 
 }
