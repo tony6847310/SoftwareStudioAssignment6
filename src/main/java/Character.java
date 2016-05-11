71f3b00c25318c175e7702964ff70ceaa049dbea
PoHaoHsu <haohsu0823@gmail.com>
2016-05-11 22:03:16 +0800

    add mouse control

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -10,11 +10,10 @@ public class Character {
 	
 	private MainApplet parent;
 	
-	private static final float ellipse_X = 575, ellipse_Y = 340;
 	private int color;
 	private float ori_X,ori_Y;
-	private float cur_X,cur_Y;
-	private float radius = 25;
+	public float cur_X,cur_Y;
+	public float radius = 25;
 	private ArrayList<Character> targets;
 	private ArrayList<Integer> scenes;
 	private boolean activated;
@@ -34,23 +33,8 @@ public class Character {
 	}
 
 	public void display(){
-		this.parent.noStroke();
 		this.parent.fill(color);
 		this.parent.ellipse(cur_X, cur_Y, radius*2 , radius*2 );
-		//If this node is activated , draw connect lines with other nodes
-		if(activated){
-			for(int i=0 ; i<targets.size(); i++){
-				if(targets.get(i).checkActivated()){
-					float s1 = (ellipse_X + (cur_X + targets.get(i).cur_X) / 2) / 2;
-					float s2 = (ellipse_Y + (cur_Y + targets.get(i).cur_Y) / 2) / 2;
-					parent.noFill();
-					parent.stroke(51, 51, 51);
-					parent.strokeWeight(scenes.get(i) / 10 + 1);
-					parent.bezier(cur_X, cur_Y, s1, s2, s1, s2, 
-							targets.get(i).cur_X, targets.get(i).cur_Y);
-				}
-			}
-		}
 	}
 	
 	public void addTarget(Character target,Integer value){
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -6,8 +6,8 @@ import processing.data.JSONObject;
 import controlP5.ControlP5;
 import ddf.minim.Minim;
 import de.looksgood.ani.Ani;
-
 import java.awt.event.KeyEvent;
+
 import java.util.*;
 
 /**
@@ -15,7 +15,7 @@ import java.util.*;
 * You can do major UI control and some visualization in this class.  
 */
 @SuppressWarnings("serial")
-public class MainApplet extends PApplet{
+public class MainApplet extends PApplet {
 	private String path = "main/resources/";
 	private String fileNameLeft = "starwars-episode-";
 	private String fileNameRight = "-interactions.json";
@@ -28,7 +28,8 @@ public class MainApplet extends PApplet{
 	private JSONArray[] links = new JSONArray[8];
 	private ArrayList<Character> characters;
 	private int episode = 1;
-	
+	private boolean pointingnode;
+	private Character pointednode, pointingnodebutpressed;
 	private Minim mn;
 	private ControlP5 cp;
 	private Ani ani;
@@ -50,18 +51,43 @@ public class MainApplet extends PApplet{
 		background(255);
 		//draw the circle
 		fill(255);
-		stroke(83, 198, 140);
+		stroke(38, 58, 109);
 		strokeWeight(5);
 		ellipse(575, 340, 520, 520);
 		//display text
 		fill(100, 50, 25);
-		textSize(30);
-		text("Star Wars episode" + Integer.toString(episode), 440, 55);
+		textSize(26);
+		text("Star Wars episode" + Integer.toString(episode), 450, 70);
 		//draw character nodes
 		for(Character c: this.characters){
 			c.display();
+			if(dist(c.cur_X, c.cur_Y, mouseX, mouseY) < c.radius && !mousePressed){
+				pointingnodebutpressed = c;
+			}
+			else {
+				ani = Ani.to(c, (float)0.5, "radius", c.radius);
+			}
+		}
+		for(Character c: this.characters){
+			if(dist(c.cur_X, c.cur_Y, mouseX, mouseY) < c.radius){
+				pointingnode = true;
+				break;
+			}
+			else {
+				pointingnode = false;
+			}
 		}
 		
+		if(pointingnodebutpressed != null){
+			if(dist(pointingnodebutpressed.cur_X, pointingnodebutpressed.cur_Y, mouseX, mouseY) < pointingnodebutpressed.radius){
+				ani = Ani.to(pointingnodebutpressed, (float)0.5, "radius", 30);
+				fill(0, 255, 187);
+				rect(mouseX, mouseY-20, pointingnodebutpressed.getName().length()*20, 40, 20);
+				fill(255, 213, 0);
+				textSize(20);
+				text(pointingnodebutpressed.getName(), mouseX+10, mouseY+5);
+			}
+		}
 	}
 
 	private void loadData(){
@@ -81,7 +107,7 @@ public class MainApplet extends PApplet{
 			int color = unhex(node.getString("colour").substring(1));
 			//convert hex string to int
 			characters.add(new Character(this, node.getString("name"), color, i%4*60 +50, i/4*60 + 50));
-			//add 4 characters each row
+			//add 4 character each row
 		}
 		
 		for(int i=0; i<links[episode].size(); i++){
@@ -90,26 +116,7 @@ public class MainApplet extends PApplet{
 			//record scenes between each character
 		}
 	}
-	
-	public void keyPressed(){
-		if(keyCode == KeyEvent.VK_1)
-			episode = 1;
-		else if(keyCode == KeyEvent.VK_2)
-			episode = 2;
-		else if(keyCode == KeyEvent.VK_3)
-			episode = 3;
-		else if(keyCode == KeyEvent.VK_4)
-			episode = 4;
-		else if(keyCode == KeyEvent.VK_5)
-			episode = 5;
-		else if(keyCode == KeyEvent.VK_6)
-			episode = 6;
-		else if(keyCode == KeyEvent.VK_7)
-			episode = 7;
-		
-		loadEpisode();
-	}
-	
+
 	public void addAll() {
 		for(Character c: characters) {
 			c.activate(true);
@@ -121,8 +128,36 @@ public class MainApplet extends PApplet{
 	public void clear() {
 		for(Character c: characters) {
 			c.activate(false);
+			ani = Ani.to(c, (float)1, "cur_X", c.getOriPosX());
+			ani = Ani.to(c, (float)1, "cur_Y", c.getOriPosY());
 		}
-		rearrange();
+		//rearrange();
+	}
+	
+	public void mousePressed(){
+		if(pointingnode)
+			pointednode = pointingnodebutpressed;
+	}
+	
+	public void mouseDragged(){
+		if(pointednode != null){
+			pointednode.cur_X = mouseX;
+			pointednode.cur_Y = mouseY;
+		}
+	}
+	
+	public void mouseReleased(){
+		if(pointednode != null){
+			if(dist(pointednode.cur_X, pointednode.cur_Y, 575, 340) < 260){
+				pointednode.activate(true);
+			} else {
+				pointednode.activate(false);
+				ani = Ani.to(pointednode, (float)0.5, "cur_X", c.getOriPosX());
+				ani = Ani.to(pointednode, (float)0.5, "cur_Y", c.getOriPosY());
+			}
+			rearrange();
+		}
+		pointednode = null;
 	}
 	
 	private void rearrange(){
